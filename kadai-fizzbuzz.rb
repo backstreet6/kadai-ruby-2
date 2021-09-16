773c37199e92e30d1ef6572575778d0c7c1b15d1
satoshi.fujita <not_rishi@yahoo.co.jp>
2021-09-16 02:54:33 +0000

    kadai-ruby-2

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,15 @@
+num_max = 100
+def fizzbuzz(num)
+        if num % 3 == 0
+            return "Fizz"
+        elsif num % 5 == 0
+            return "Buzz"
+        elsif num % 3 == 0 && num % 5 == 0
+            return "FizzBuzz"
+        else
+            return num
+        end
+    end
+    (1..num_max).each do |num|
+    puts fizzbuzz(num)
+end
